ccb8d570eefbbcbd08e75a6a0ecdd33c43ad5499
ritesh <riteshsutariya94@gmail.com>
2022-10-13 18:22:52 +0530

    typescript learning

---
+++ first.ts
@@ -0,0 +1,17 @@
+interface User {
+  name: string;
+  age: number;
+  gender: string;
+  address: string;
+}
+
+const user: User = {
+  name: "ritesh",
+  age: 23,
+  gender: "Male",
+  address: "surat city(EAST)",
+};
+
+let msg = "Message From TypeScript";
+
+export {};
