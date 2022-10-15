ed1a666d4c0bf806d38e88963d14c47d7b4f2fe5
ritesh <riteshsutariya94@gmail.com>
2022-10-15 11:21:54 +0530

    array

---
+++ array.js
@@ -0,0 +1,10 @@
+var GroupA = ["England", "Australia", "New Zealand", "Afghanistan"];
+var GroupB = ["India", "South Africa", "Pakistan", "Bangladesh"];
+console.log("Group A: ", GroupA);
+console.log("Group B: ", GroupB);
+GroupA.push("West Indies");
+// GroupA.push('West Indies')
+GroupB.push("Sri Lanka");
+// GroupB.push('West Indies')
+console.log("Group A: ", GroupA);
+console.log("Group B: ", GroupB);
---
+++ array.ts
@@ -0,0 +1,13 @@
+let GroupA: string[] = ["England", "Australia", "New Zealand", "Afghanistan"];
+let GroupB: string[] = ["India", "South Africa", "Pakistan", "Bangladesh"];
+
+console.log("Group A: ", GroupA);
+console.log("Group B: ", GroupB);
+
+GroupA.push("West Indies");
+// GroupA.push('West Indies')
+GroupB.push("Sri Lanka");
+// GroupB.push('West Indies')
+
+console.log("Group A: ", GroupA);
+console.log("Group B: ", GroupB);
